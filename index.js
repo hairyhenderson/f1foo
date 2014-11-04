6a939c85b6192176f52d77fafbea10f2c9601b5d
Dave Henderson <dhenderson@gmail.com>
2014-11-03 21:11:40 -0500

    cool faces

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
+var cool = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send(cool())
 })
 
 app.listen(app.get('port'), function() {
