1f83f32f0905ba5c17914044db02820ce1300c71
Dave Henderson <dhenderson@gmail.com>
2014-11-03 21:53:29 -0500

    cool faces

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var express = require('express')
 var cool = require('cool-ascii-faces');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.VCAP_APP_PORT || process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send(cool())
 })
 
+console.log('hello world')
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
