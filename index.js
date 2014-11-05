40e0009691281def6b12c0f3dc4177eccf424229
Dave Henderson <dhenderson@gmail.com>
2014-11-05 14:01:45 -0500

    WebHook support

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 var express = require('express')
-var cool = require('cool-ascii-faces');
-var app = express();
+var cool = require('cool-ascii-faces')
+var bodyParser = require('body-parser')
+var app = express()
 
 app.set('port', (process.env.VCAP_APP_PORT || process.env.PORT || 5000))
+
+app.use(bodyParser.urlencoded())
+
 app.use(express.static(__dirname + '/public'))
+
 
 app.get('/', function(request, response) {
   response.send(cool())
+})
+
+app.post('/', function(req, res) {
+  if (req.body) {
+    console.log('got a body: %j', body)
+  }
+  res.status(200).send('Thanks!')
 })
 
 console.log('hello world')
