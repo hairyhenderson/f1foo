550d812230fda0b99c0f5b5b91f2100c8f7b9efb
Dave Henderson <dhenderson@gmail.com>
2014-11-06 08:02:06 -0500

    Changing endpoint to /hooks

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ app.set('port', (process.env.VCAP_APP_PORT || process.env.PORT || 5000))
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(morgan('combined'))
-// app.use(express.static(__dirname + '/public'))
 
-app.use('/', require('./routes/hooks'))
+app.use('/hooks', require('./routes/hooks'))
 
-console.log('hello world')
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
