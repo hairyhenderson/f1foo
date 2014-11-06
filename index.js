89118efdae48f72082cc0d356b90a38ff1118208
Dave Henderson <dhenderson@gmail.com>
2014-11-06 08:32:32 -0500

    Adding empty response for /

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.set('port', (process.env.VCAP_APP_PORT || process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(morgan('combined'))
 
+app.use('/', require('./routes/index'))
 app.use('/hooks', require('./routes/hooks'))
 
 app.listen(app.get('port'), function() {
